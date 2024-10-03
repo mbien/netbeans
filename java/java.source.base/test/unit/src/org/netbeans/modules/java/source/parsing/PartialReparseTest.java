e68fc1678d95ede1086e577bd35252a4c2a5b692
Michael Bien <mbien42@gmail.com>
2024-10-03 03:22:25 +0200

    Revert "Switch from memory fs to plain old folders."
    This reverts commit 5b18e7e22715b09fc6414e3b1d9da4801f7a1aa1.

--- java/java.source.base/test/unit/src/org/netbeans/modules/java/source/parsing/PartialReparseTest.java
+++ java/java.source.base/test/unit/src/org/netbeans/modules/java/source/parsing/PartialReparseTest.java
@@ -71,7 +71,6 @@ public class PartialReparseTest extends NbTestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        clearWorkDir();
         SourceUtilsTestUtil.prepareTest(new String[0], new Object[0]);
         // ensure JavaKit is present, so that NbEditorDocument is eventually created.
         // it handles PositionRefs differently than PlainDocument/PlainEditorKit.
@@ -386,7 +385,7 @@ public class PartialReparseTest extends NbTestCase {
 
     private void doRunTest(String code, String inject, Consumer<CompilationInfo> callback) throws Exception {
 
-        FileObject srcDir = FileUtil.createFolder(getWorkDir());
+        FileObject srcDir = FileUtil.createMemoryFileSystem().getRoot();
         FileObject src = srcDir.createData("Test.java");
 
         // parse original source
@@ -438,7 +437,7 @@ public class PartialReparseTest extends NbTestCase {
 
     private void doVerifyFullReparse(String code, String inject) throws Exception {
 
-        FileObject srcDir = FileUtil.createFolder(getWorkDir());
+        FileObject srcDir = FileUtil.createMemoryFileSystem().getRoot();
         FileObject src = srcDir.createData("Test.java");
 
         // parse original source
